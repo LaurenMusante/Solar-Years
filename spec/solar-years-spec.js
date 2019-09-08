c9cee9fffd470b099f542f76ece8383408d9de15
Lauren <Lauren.Musante@gmail.com>
2019-09-08 15:47:31 -0700

    Comment out a bunch of extra coding. Edit package.json to remove a single comma to get Karma to start working again. Run 3 tests.

--- spec/solar-years-spec.js
+++ spec/solar-years-spec.js
@@ -5,66 +5,64 @@ import { calcMarsAge } from './../src/solar-years.js';
 import { calcJupiterAge } from './../src/solar-years.js';
 
 
-describe ('Age', function() {
+describe ('Age', function(){
   it('should not accept non-numerical input', function() {
-    let exampleAge = new Age ("peanut");
-    expect (exampleAge.checkValidity()).toBe(false);
+    let exampleAge = new Age("peanut");
+    expect(exampleAge.checkValidity()).toBe(false);
+  });
+  it('should not accept non-numerical input', function(){
+    let exampleAge2 = new Age(33);
+    expect(exampleAge2.checkValidity()).toBe(true);
   });
 });
 
-describe ('Age', function() {
-  it('should not accept non-numerical input'), function(){
-    let exampleAge2 = new Age (33);
-    expect (exampleAge2.checkValidity()).toBe(false)
-  });
-});
 
 describe ('calcMercuryAge', function(){
   it('should convert inputted Earth age into Mercury age', function() {
-    let age = "33";
-     expect(calcMercuryAge(age)).toEqual(138);
+    let exampleAge3 = new Age(33);
+     expect(exampleAge3.calcMercuryAge(exampleAge3.age)).toEqual(137.5);
   });
 });
-
-describe ('calcVenusAge'), function() {
-  it('should convert inputted Earth age into Venus age using simple calculation', function() {
-    let age = "33";
-    expect (calcVenusAge(age)).toEqual(53.22)
-  });
-});
-
-describe ('calcMarsAge'), function() {
-  it('should convert inputted Earth age into Mars age using simple calculation', function() {
-    let age = "33";
-    expect (calcMarsAge(age)).toEqual(17.55)
-  });
-});
-
-describe ('calcJupiterAge'), function() {
-  it('should convert inputted Earth age into Jupiter age using simple calculation', function() {
-    let age = "33";
-    expect (calcJupiterAge(age)).toEqual(2.78)
-  });
-});
-
-describe ('expectancy'), function() {
-  it('should calculate life expectancy based on which continent is selected from dropdown  menu')
-    let exampleExpectancy = africa;
-    expect (expectancy(africa).toEqual(62.5)
-  });
-});
-
-describe ('remainingYears'), function() {
-  it('should calcuate remaining years in life based on a the age of user and continental residence')
-    let lifeExpectancy = 72.5
-    let age = 33
-    expect (remainingYears().toEqual)(39.5)
-  });
-});
-
-describe ('mercuryTimeRemaining'), function() {
-  it('should convert timeRemaining from Earth years to Mercury years')
-    let timeRemaining = 39.5
-    expect (mercuryTimeRemaining(timeRemaining).toEqual)(164.58)
-  });
-});
+//
+// describe ('calcVenusAge'), function() {
+//   it('should convert inputted Earth age into Venus age using simple calculation', function() {
+//     let age = "33";
+//     expect (calcVenusAge(age)).toEqual(53.22)
+//   });
+// });
+//
+// describe ('calcMarsAge'), function() {
+//   it('should convert inputted Earth age into Mars age using simple calculation', function() {
+//     let age = "33";
+//     expect (calcMarsAge(age)).toEqual(17.55)
+//   });
+// });
+//
+// describe ('calcJupiterAge'), function() {
+//   it('should convert inputted Earth age into Jupiter age using simple calculation', function() {
+//     let age = "33";
+//     expect (calcJupiterAge(age)).toEqual(2.78)
+//   });
+// });
+//
+// describe ('expectancy'), function() {
+//   it('should calculate life expectancy based on which continent is selected from dropdown  menu')
+//     let exampleExpectancy = africa;
+//     expect (expectancy(africa).toEqual(62.5)
+//   });
+// });
+//
+// describe ('remainingYears'), function() {
+//   it('should calcuate remaining years in life based on a the age of user and continental residence')
+//     let lifeExpectancy = 72.5
+//     let age = 33
+//     expect (remainingYears().toEqual)(39.5)
+//   });
+// });
+//
+// describe ('mercuryTimeRemaining'), function() {
+//   it('should convert timeRemaining from Earth years to Mercury years')
+//     let timeRemaining = 39.5
+//     expect (mercuryTimeRemaining(timeRemaining).toEqual)(164.58)
+//   });
+// });
--- src/solar-years.js
+++ src/solar-years.js
@@ -1,88 +1,90 @@
 export class Age {
   constructor(age) {
-    this.age = earthAge;
+    this.age = age;
   }
 checkValidity() {
-  let age = false;
+  let age;
   let invalidLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
   for(let i=0; i< invalidLetters.length; i++) {
-    if (this.age.includes(invalidLetters[i])) {
-      console.log("Please enter your age using only numbers");
+    if (this.age.toString().includes(invalidLetters[i])) {
       return false;
-      }
+    } else {
+      age = true;
     }
   }
-};
-export function calcMercuryAge() {
-  let age = parseInt($('input:text').val());
-  let mercuryAge = (age / 0.24);
-  return mercuryAge;
-}
-export function calcVenusAge() {
-  let age = parseInt($('input:text').val());
-  let mercuryAge = (age / 0.62);
-  return venusAge;
-}
-export function calcMarsAge() {
-  let age = parseInt($('input:text').val());
-  let mercuryAge = (age / 1.88 );
-  return marsAge;
-}
-export function calcJupiterAge() {
-  let age = parseInt($('input:text').val());
-  let mercuryAge = (age / 11.86);
-  return jupiterAge;
-}
-
-$(document).ready(function(){
-  $("form#ageContinent").submit(function(event){
-    event.preventDefault();
-
-let africa = $("select[value=africa") option:selected").val();
-let asia = $("select[value=asia") option:selected").val();
-let europe = $("select[value=europe") option:selected").val();
-let latinAmerica = $("select[value=latinAmerica") option:selected").val();
-let northAmerica = $("select[value=northAmerica") option:selected").val();
-let oceania = $("select[value=oceania") option:selected").val();
-
-function expectancy(){
-  let lifeExpectancy = 0
-  if (africa) {
-    lifeExpectancy += 62.5
-  } else if (asia) {
-    lifeExpectancy += 72.5
-  } else if (europe) {
-    lifeExpectancy += 78.5
-  } else if (latinAmerica) {
-    lifeExpectancy += 76
-  } else if (northAmerica) {
-    lifeExpectancy += 79
-  } else {
-    lifeExpectancy += 78
+  return age;
+  }
+  calcMercuryAge(age) {
+    let mercuryAge = age / 0.24;
+    return mercuryAge;
   };
-}
-
-function remainingYears() {
-  let timeRemaining = ("lifeExpectancy" - "age")
 };
-remainingYears(age);
 
-function mercuryTimeRemaining() {
-  timeRemaining/0.24
-};
-mercuryTimeRemaining(timeremaining);
-
-function venusTimeRemaining() {
-  timeRemaining/0.62
-};
-venusTimeRemaining(timeremaining);
-
-function marsTimeRemaining() {
-  timeRemaining/1.88
-}
-marsTimeReamining(timeremaining);
-
-function jupiterTimeRemaining() {
-  timeRemaining/11.86
-}
-jupiterTimeRemaining(timeremaining);
+// export function calcVenusAge() {
+//   let age = parseInt($('input:text').val());
+//   let mercuryAge = (age / 0.62);
+//   return venusAge;
+// }
+// export function calcMarsAge() {
+//   let age = parseInt($('input:text').val());
+//   let mercuryAge = (age / 1.88 );
+//   return marsAge;
+// }
+// export function calcJupiterAge() {
+//   let age = parseInt($('input:text').val());
+//   let mercuryAge = (age / 11.86);
+//   return jupiterAge;
+// }
+//
+// $(document).ready(function(){
+//   $("form#ageContinent").submit(function(event){
+//     event.preventDefault();
+//
+// let africa = $("select[value=africa] option:selected").val();
+// let asia = $("select[value=asia] option:selected").val();
+// let europe = $("select[value=europe] option:selected").val();
+// let latinAmerica = $("select[value=latinAmerica] option:selected").val();
+// let northAmerica = $("select[value=northAmerica] option:selected").val();
+// let oceania = $("select[value=oceania] option:selected").val();
+//
+// function expectancy(){
+//   let lifeExpectancy = 0
+//   if (africa) {
+//     lifeExpectancy += 62.5
+//   } else if (asia) {
+//     lifeExpectancy += 72.5
+//   } else if (europe) {
+//     lifeExpectancy += 78.5
+//   } else if (latinAmerica) {
+//     lifeExpectancy += 76
+//   } else if (northAmerica) {
+//     lifeExpectancy += 79
+//   } else {
+//     lifeExpectancy += 78
+//   };
+// }
+//
+// function remainingYears() {
+//   let timeRemaining = ("lifeExpectancy" - "age")
+// };
+// remainingYears(age);
+//
+// function mercuryTimeRemaining() {
+//   timeRemaining/0.24
+// };
+// mercuryTimeRemaining(timeremaining);
+//
+// function venusTimeRemaining() {
+//   timeRemaining/0.62
+// };
+// venusTimeRemaining(timeremaining);
+//
+// function marsTimeRemaining() {
+//   timeRemaining/1.88
+// }
+// marsTimeReamining(timeremaining);
+//
+// function jupiterTimeRemaining() {
+//   timeRemaining/11.86
+// }
+// jupiterTimeRemaining(timeremaining);
